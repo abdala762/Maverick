67fc7558cf2b9272ae122879b0eb837b3382aca6
Gustavo Lopes <gudominguete@gmail.com>
2020-03-27 11:42:59 -0300

    Implementação de verificação se duas listas são iguais no teste de API

--- Source/Maverick.Application.ApiTest/Controllers/FilmesControllerTeste/GetFilmesAsyncTeste.cs
+++ Source/Maverick.Application.ApiTest/Controllers/FilmesControllerTeste/GetFilmesAsyncTeste.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,12 +17,13 @@ using Xunit;
 
 namespace Maverick.Application.ApiTest.Controllers.FilmesControllerTeste
 {
-      public class GetFilmesAsyncTeste {
+    public class GetFilmesAsyncTeste
+    {
 
         private readonly FilmesController filmesController;
         private readonly Mock<IFilmesService> filmeServiceMock;
         private readonly Mock<IMapper> mapper = new Mock<IMapper>();
-    
+
 
         public GetFilmesAsyncTeste()
         {
@@ -34,10 +36,21 @@ namespace Maverick.Application.ApiTest.Controllers.FilmesControllerTeste
         [Fact]
         public async Task ObterFilmesAsync_Sucesso()
         {
-            
-            var expected = new List<FilmesGetResult>()
+            var expected = new List<FilmesGetResult>
                 {
-                    new FilmesGetResult()
+                    new FilmesGetResult
+                    {
+                        Id = 10448,
+                        Descricao = "descricao_test_2",
+                        Nome = "nome_teste_2"
+                    },
+                    new FilmesGetResult
+                    {
+                        Id = 10449,
+                        Descricao = "descricao_test_12",
+                        Nome = "nome_teste_3"
+                    },
+                    new FilmesGetResult
                     {
                         Id = 10447,
                         Descricao = "descricao_teste",
@@ -47,37 +60,55 @@ namespace Maverick.Application.ApiTest.Controllers.FilmesControllerTeste
 
             mapper.Setup(m => m.Map<IEnumerable<FilmesGetResult>>(It.IsAny<IEnumerable<Filme>>())).Returns(expected);
 
-
             var filmes = await filmesController.GetFilmesAsync(new FilmesGet
             {
                 TermoPesquisa = "a",
                 AnoLancamento = 2012
-            });          
+            });
 
             //Assert
             var viewResult = Assert.IsType<OkObjectResult>(filmes);
             //Assert garantindo que variavel filmes tem os campos e tipos de campos exatamente iguais a classe FilmeModelTeste
             Assert.IsAssignableFrom<List<FilmesGetResult>>(viewResult.Value);
-            var listaFilme = (List<FilmesGetResult>)viewResult.Value;
-            var filme = listaFilme[0];
+            var listaFilmeRetornado = (List<FilmesGetResult>)viewResult.Value;
+          
+            var listaFilmeEsperado = new List<FilmeModelTeste>
+            {
+                new FilmeModelTeste
+                {
+                    Id = 10447,
+                    Descricao = "descricao_teste",
+                    Nome = "nome_teste"
+                },
+                new FilmeModelTeste
+                {
+                    Id = 10448,
+                    Descricao = "descricao_test_2",
+                    Nome = "nome_teste_2"
+                },
+                new FilmeModelTeste
+                {
+                    Id = 10449,
+                    Descricao = "descricao_test_12",
+                    Nome = "nome_teste_3"
+                }
+            };
 
-            TesteUtil.VerificarClassesIguais(filme.GetType(), filme.GetType());
+            TesteUtil.VerificarListasClassesIguais(listaFilmeEsperado, listaFilmeRetornado);
         }
-
-      
     }
 
     public class FilmeModelTeste
     {
         /// <summary>
+        /// Nome do filme.
+        /// </summary>
+        public string Nome { get; set; }
+        /// <summary>
         /// Identificador do filme no The Moview Database.
         /// </summary>
         public long Id { get; set; }
 
-        /// <summary>
-        /// Nome do filme.
-        /// </summary>
-        public string Nome { get; set; }
 
         /// <summary>
         /// Descricao/Resumo do filme.
